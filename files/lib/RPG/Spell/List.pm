21a0b7032fd6e4a90fbe3c62dc21e187e0617096
Lady Aleena <LadyAleena@users.noreply.github.com>
2020-07-03 15:48:56 -0400

    Remove use of IO::All
    - removed use of IO::All at the suggestion of rindolf
    
    - added spell_description_from_file subroutine to replace it

--- files/lib/RPG/Spell/List.pm
+++ files/lib/RPG/Spell/List.pm
@@ -5,13 +5,19 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(spell_data);
 
 use CGI::Carp qw(fatalsToBrowser);
-use IO::All;
 
 use Util::Convert qw(filify);
 use Util::Data qw(data_file make_hash);
 
 my @headings = (qw(name school level range duration), 'area of effect', 'components', 'casting time', 'saving throw', 'description');
 my $spells = make_hash( 'file' => ['Role_playing','spell_list.txt'], 'headings' => \@headings );
+
+sub spell_description_from_file {
+  my $spell_file = shift;
+  open(my $spell_fh, '<:encoding(utf-8)', $spell_file) || die "Can't open $spell_file. $!";
+  my $spell_description = do { local $/; readline($spell_fh) };
+  return $spell_description;
+}
 
 sub spell_data {
   my ($spell) = @_;
@@ -26,7 +32,7 @@ sub spell_data {
   my $spell_file = filify($spell);
   my @spell_description = $spells->{$spell}{'description'} ?
                           split(/\//, $spells->{$spell}{'description'}) :
-                          io(data_file('Role_playing/spell_descriptions',"$spell_file.txt"))->slurp;
+                          spell_description_from_file(data_file('Role_playing/spell_descriptions',"$spell_file.txt"));
 
   my $spell_out = { 'heading' => $spells->{$spell}{'name'}, 'stats' => [\@items, { class => 'spell_stats' }], 'description' => \@spell_description };
   return $spell_out;
