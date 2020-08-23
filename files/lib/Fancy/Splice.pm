9a96c24b53d56d47b8382e541eb524aac34dc70f
Lady Aleena <LadyAleena@users.noreply.github.com>
2020-08-23 14:43:35 -0400

    Move POD from Splice.pm to Splice.pod
    GitHub displays pod with markup for easier reading.

--- files/lib/Fancy/Splice.pm
+++ files/lib/Fancy/Splice.pm
@@ -17,53 +17,7 @@ sub fancy_splice {
   return \@list;
 }
 
-=pod
-
-=encoding utf8
-
-=head1 NAME
-
-B<Fancy::Splice> splices an array into groups.
-
-=head1 VERSION
-
-This document describes Fancy::Splice version 1.0.
-
-=head1 SYNOPSIS
-
-  my @colors = ('red', 'orange', 'yellow', 'spring', 'green', 'teal', 'cyan', 'azure',
-                'blue', 'violet', 'magenta', 'pink', 'white', 'black', 'gray');
-  my $list = fancy_splice(2, @colors);
-
-  [
-    ['red','orange'],
-    ['yellow','spring'],
-    ['green','teal'],
-    ['cyan','azure'],
-    ['blue','violet'],
-    ['magenta','pink'],
-    ['white','black'],
-    [gray']
-  ];
-
-=head1 DESCRIPTION
-
-C<fancy_splice> can be exported and returns a referenced array of arrays spliced by a user specified amount. It takes two paraments: the size of the groups and the list. The size can be any integer, and the list is an array.
-
-=head1 DEPENDENCY
-
-Fancy::Splice depends on L<Exporter>.
-
-=head1 AUTHOR
-
-Lady Aleena
-
-=head1 LICENSE AND COPYRIGHT
-
-This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself. See L<perlartistic>.
-
-Copyright © 2020, Lady Aleena C<<aleena@cpan.org>>. All rights reserved.
-
-=cut
+# This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself. See https://dev.perl.org/licenses/artistic.html.
+# Copyright © 2020, Lady Aleena (aleena@cpan.org). All rights reserved.
 
 1;
