dc480bd6e9eaed244f92583b2d92a1d7eadf3e67
Lady Aleena <LadyAleena@users.noreply.github.com>
2020-08-29 20:32:14 -0400

    Add ability to float right with 'right' option

--- files/lib/Page/Story/Magic/FamilyTree.pm
+++ files/lib/Page/Story/Magic/FamilyTree.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Exporter qw(import);
 
+use List::Util qw(any);
+
 use Page::File qw(file_list);
 use Page::HTML qw(object figure anchor);
 use Page::Line qw(line);
@@ -13,14 +15,17 @@ our $VERSION   = "1.0";
 our @EXPORT_OK = qw(family_tree_magic);
 
 sub family_tree_magic {
-  my $trees_dir = shift;
+  my ($trees_dir, $opt) = @_;
   my $magic;
 
   my @trees_list = file_list($trees_dir);
+  my @right_images = $opt->{right} ? @{$opt->{right}} : ();
   for my $tree_file (@trees_list) {
-    my $link = "$trees_dir/$tree_file";
+    my $text  = textify($tree_file);
+    my $right = any { /\b$text\b/i } @right_images;
+    my $link  = "$trees_dir/$tree_file";
     my $class = 'svg_group';
-    my $text = textify($tree_file);
+       $class .= ' right' if $right;
     my ($family, $source) = split(/ /, $text, 2);
     my $title = $family && $source ? "The $family family from $source." : $text;
 
